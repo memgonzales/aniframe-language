aac8709e0b9a7fa249f9b9b2dfc87f743ea5bb65
memgonzales <gonzales.markedward@gmail.com>
2023-11-26 16:41:35 +0800

    Fix mapping for curve

--- interpreter/mapping.py
+++ interpreter/mapping.py
@@ -179,8 +179,8 @@ def convert_point_to_p5(name, x, y):
 def convert_line_to_p5(name, x1, y1, x2, y2):
     convert_shape_to_p5(clean_identifier(name), f'line({x1}, {y1}, {x2}, {y2})')
 
-def convert_curve_to_p5(name, x1, y1, x2, y2, x3, y3):
-    convert_shape_to_p5(clean_identifier(name), f'curve({x1}, {y1}, {x2}, {y2}, {x3}, {y3})')
+def convert_curve_to_p5(name, x1, y1, x2, y2, x3, y3, x4, y4):
+    convert_shape_to_p5(clean_identifier(name), f'curve({x1}, {y1}, {x2}, {y2}, {x3}, {y3}, {x4}, {y4})')
 
 def convert_circle_to_p5(name, x, y, radius):
     convert_shape_to_p5(clean_identifier(name), f'circle({x}, {y}, {radius})')
